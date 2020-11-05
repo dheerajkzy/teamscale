292aa9286e5c37898587a68bfc772a75ae263827
Dheeraj K <dheeraj.k@kpit.com>
2020-11-05 13:20:51 +0100

    Example cpp

---
+++ corrected_my_fuzz.cpp
@@ -0,0 +1,16 @@
+#include <cstdint>
+
+std::size_t find_new_line(const char *str,const std::size_t size)
+{
+	std::size_t loc = 0;
+	while((loc < size) && (str[loc] != '\n'))
+	{
+		++loc;
+	}
+	return loc;
+}
+
+extern "C" int LLVMFuzzerTestOneInput(const char *str, const std::size_t size) {
+  find_new_line(str,size);
+  return 0;
+}
